1b7abfbd1616838f7c6ea3784b7b645e805e45f4
maxcorad <80256615+maxcorad@users.noreply.github.com>
2021-03-22 22:59:43 +0100

    update: my solution

--- prob_calculator.py
+++ prob_calculator.py
@@ -3,6 +3,24 @@ import random
 # Consider using the modules imported above.
 
 class Hat:
+    def __init__(self, **kwargs):
+        contents = [arg for arg in kwargs for _ in range(kwargs[arg])]
+        self.contents = contents
+
+    def draw(self, number):
+        return [
+            self.contents.pop(random.randint(0, len(self.contents) - 1))
+            for _ in range(number)
+        ] if number < len(self.contents) else self.contents
 
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    matches = 0
+    for _ in range(num_experiments):
+        h = copy.deepcopy(hat)
+        draw = h.draw(num_balls_drawn)
+        matches += 1 if all([
+            draw.count(color) >= expected_balls[color]
+            for color in expected_balls
+        ]) else 0
+    return matches / num_experiments
